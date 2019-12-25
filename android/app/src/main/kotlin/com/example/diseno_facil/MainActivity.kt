57009f47eb29ab2e9c83a633c743d5efacf22309
jorgeroma <jorgeroma2000@gmail.com>
2019-12-25 19:19:22 +0100

    commit inicial

---
+++ android/app/src/main/kotlin/com/example/diseno_facil/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.diseno_facil
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
